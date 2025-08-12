0754edb7e12286c81f91f3ac841ca9577322d89f
RISHIKESH SRIRAM V <rishisrrm@gmail.com>
2025-08-12 23:54:35 +0530

    Update Songsscript.js

--- Songsscript.js
+++ Songsscript.js
@@ -23,7 +23,7 @@ const songs = [
   },
   {
     title: "SAY_NO_TO_DRUGS",
-    src: "Songs/Onnuku-Renda-MassTamilan.org.mp3",
+    src: "Say no to drugs karokr.mp3",
     cover: "client/Songsimages/WhatsApp_Image_2025-08-02_at_11.54.48_7f11c120-removebg-preview.png",
     musicBy: "RISHIKESH SRIRAM",
     lyrics: "NADODI KAVINGAN PUSHPARAJ",
@@ -97,6 +97,7 @@ songs.forEach((song, index) => {
   });
   playlist.appendChild(li);
 });
+
 
 
 
